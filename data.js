3fcd4bcc6c8506d6a0519e98ecc219c877baec9a
lou-flan <louise_flanagan@outlook.com>
2020-06-03 18:23:23 +1000

    added an animation between ball click and card display to allow for image load time. changed background to radial gradient and created SVG for ball instead of CSS

---
+++ data.js
@@ -0,0 +1,20 @@
+const pokemonTypeColours = {
+	ground: "#e7d39f",
+	electric: "#fdd998",
+	bug: "#cee397",
+	dark: "#222831",
+	dragon: "#8ac6d1",
+	fairy: "#ffb6b9",
+	fighting: "#ffc38b",
+	fire: "#ff6363",
+	flying: "#a4c5c6",
+	ghost: "#827397",
+	grass: "#b7efcd",
+	ice: "#dae1e7",
+	normal: "#eae7d9",
+	poison: "#8566aa",
+	psychic: "#efa8e4",
+	rock: "#d2c6b2",
+	steel: "#5f6769",
+	water: "#9aceff"
+};
--- script.js
+++ script.js
@@ -1,74 +1,49 @@
-const pokemonTypeColours = {
-	ground: "#e7d39f",
-	electric: "#fdd998",
-	bug: "#cee397",
-	dark: "#222831",
-	dragon: "#8ac6d1",
-	fairy: "#ffb6b9",
-	fighting: "#ffc38b",
-	fire: "#ff6363",
-	flying: "#a4c5c6",
-	ghost: "#827397",
-	grass: "#b7efcd",
-	ice: "#dae1e7",
-	normal: "#eae7d9",
-	poison: "#8566aa",
-	psychic: "#efa8e4",
-	rock: "#d2c6b2",
-	steel: "#5f6769",
-	water: "#9aceff"
-};
-var cardDiv = document.getElementById("characters");
-var pokeBall = document.getElementById("pokeball-container");
+let cardDiv = document.getElementById("card");
+let pokeBall = document.getElementById("pokeball-container");
+let button = document.getElementById("pokeball-click")
 
-function randomIdNumber() {
-	return Math.floor(Math.random() * Math.floor(307));
-}
-
-function getCharacter(event) {
+function getPokemonId(event) {
 	event.preventDefault();
-	let numberInput = document.getElementById("number-input");
-
-	var pokemonId = randomIdNumber();
+	let pokemonId = Math.floor(Math.random() * Math.floor(800));
 	if (pokemonId) {
-		getCharacters(pokemonId);
+		getPokemonData(pokemonId);
 	}
 }
 
 document
-	.getElementById("submit-number")
-	.addEventListener("click", getCharacter);
+	.getElementById("pokeball-click")
+	.addEventListener("click", getPokemonId);
 
-function getCharacters(pokemonId) {
-	fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
-		.then((response) => response.json())
-		.then((data) => showCard(data))
-		.catch((error) => console.log(error));
+const getPokemonData = async (pokemonId) => {
+	let url = 'https://pokeapi.co/api/v2/pokemon/'
+	const response = await fetch(url + pokemonId)
+	const data = await response.json()
+	const moveJson = await fetch(data.moves[0].move.url)
+	const moveData = await moveJson.json()
+	const image = await `https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`
+	await showCard(data, image)
+	await showMoveData(moveData)
 }
 
-function getMoveData(url) {
-	fetch(url)
-		.then((response) => response.json())
-		.then((data) => showMoveData(data))
-		.catch((error) => console.log(error));
-}
 
-function showCard(data) {
-	pokeBall.style.display = "none";
+const showCard = async (data, image) => {
+	let {name} = data
+	pokeBall.style.animation = "spin .3s linear infinite";
+	button.style.stroke = "#2dfdd4"
 	cardDiv.innerHTML = null;
 
-	var pokemonName = document.createElement("h2");
-	pokemonName.textContent = `${data.name.toUpperCase()}`;
+	let pokemonName = document.createElement("h2");
+	pokemonName.textContent = `${name.toUpperCase()}`;
 
 	let mediaContainer = document.createElement("div");
 	mediaContainer.setAttribute("class", "mediaContainer");
 
-	let pokemonImage = document.createElement("img");
+	let pokemonImage = await document.createElement("img");
 	pokemonImage.setAttribute(
 		"src",
-		`https://pokeres.bastionbot.org/images/pokemon/${data.id}.png`
+		image
 	);
-	pokemonImage.setAttribute("alt", data.name);
+	pokemonImage.setAttribute("alt", name);
 
 	cardDiv.appendChild(pokemonName);
 	mediaContainer.appendChild(pokemonImage);
@@ -78,27 +53,30 @@ function showCard(data) {
 	let pokemonTypeTitle = document.createElement("h3");
 	pokemonTypeTitle.textContent = pokemonType;
 	cardDiv.appendChild(pokemonTypeTitle);
-	getMoveData(data.moves[0].move.url);
 
 	setBackgroundColour(mediaContainer, pokemonType);
 	setTextColour([pokemonTypeTitle], pokemonType);
-	cardDiv.style.visibility = "visible";
+
+	waitForImageToLoad(pokemonImage).then(() => {
+		cardDiv.style.visibility = "visible";
+		pokeBall.style.animation = "wobble 2s infinite both";
+		button.style.stroke = "#424242"
+		pokeBall.style.display = "none";
+		
+	})	
+}
+
+function waitForImageToLoad(imageElement){
+	return new Promise(resolve=>{imageElement.onload = resolve})
 }
 
 function showMoveData(data) {
-	var pokemonMoves = data.name;
-	var cardBodyText = document.createElement("div");
-	var moveData = data.flavor_text_entries[2].flavor_text;
+	let pokemonMoves = data.name;
+	let cardBodyText = document.createElement("div");
+	let moveData = data.flavor_text_entries[2].flavor_text;
 	cardBodyText.innerHTML = `<p>${pokemonMoves.toUpperCase()}</p><br><p id="inner">${moveData}</p>`;
 	cardDiv.appendChild(cardBodyText);
 }
-
-function reset() {
-	cardDiv.style.visibility = "hidden";
-	pokeBall.style.display = "block";
-}
-
-cardDiv.addEventListener("click", reset);
 
 function setBackgroundColour(element, data) {
 	element.style.background = `linear-gradient(0deg, white 10%, ${pokemonTypeColours[data]})`;
@@ -109,3 +87,12 @@ function setTextColour(element, data) {
 		item.style.color = pokemonTypeColours[data];
 	});
 }
+
+function reset() {
+	cardDiv.style.visibility = "hidden";
+	pokeBall.style.display = "block";
+}
+
+cardDiv.addEventListener("click", reset);
+
+
